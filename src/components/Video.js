24376098e34c094b035b5a2e40dcc64af720c2f2
IvanSmiths <info@ivansmiths.com>
2023-03-23 23:08:45 +0100

    add bebas font

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,12 @@
+import { Bebas_Neue } from 'next/font/google'
+const bebas = Bebas_Neue({ subsets: ['latin'], weight: ['400'] })
 
 function Video() {
     return (
         <div className="video-section">
             <video src="/video.mp4" loop autoPlay muted></video>
             <div className='video-copy'>
-                <h1>RED</h1>
+                <h1 className={bebas.className}>RED</h1>
             </div>
         </div>
     )
